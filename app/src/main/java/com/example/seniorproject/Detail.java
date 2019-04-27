ea8ebf25c5f2e89129ef47892008643cb29fe50e
Gokhan <m.gokhan.yildiz@gmail.com>
2019-04-27 17:29:12 +0300

    son guncel hal

--- app/src/main/java/com/example/seniorproject/Detail.java
+++ app/src/main/java/com/example/seniorproject/Detail.java
@@ -1,6 +1,7 @@
 package com.example.seniorproject;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -31,8 +32,8 @@ public class Detail extends AppCompatActivity {
     Singleton singleton;
     List<GetField> getFieldList;
     String temp, hum, out, tempTemp, tempHum;
-    Runnable runnable;
     Handler handler;
+    Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,15 +56,14 @@ public class Detail extends AppCompatActivity {
 
     public void writeDataToDatabase() {
         handler = new Handler();
-
         runnable = new Runnable() {
             @Override
             public void run() {
                 thingSpeak();
-                handler.postDelayed(runnable, 100);
+                handler.postDelayed(runnable, 2000);
             }
         };
-       handler.post(runnable);
+        handler.postDelayed(runnable, 2000);
     }
 
     public void fill_list() {
@@ -73,7 +73,7 @@ public class Detail extends AppCompatActivity {
         call.enqueue(new Callback<List<GetField>>() {
             @Override
             public void onResponse(Call<List<GetField>> call, Response<List<GetField>> response) {
-                if(response.isSuccessful()) {
+                if (response.isSuccessful()) {
                     getFieldList = response.body();
                     Adapter adapter = new Adapter(Detail.this, getApplicationContext(), getFieldList);
                     listView.setAdapter(adapter);
@@ -96,11 +96,11 @@ public class Detail extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.add_field1) {
+        if (item.getItemId() == R.id.add_field1) {
             Intent AddFieldActivity = new Intent(getApplicationContext(), AddFieldActivity.class);
             startActivity(AddFieldActivity);
         }
-        if(item.getItemId() == R.id.logout_user) {
+        if (item.getItemId() == R.id.logout_user) {
             Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
             startActivity(intent);
         }
@@ -108,12 +108,12 @@ public class Detail extends AppCompatActivity {
     }
 
     public void thingSpeak() {
+
         Call<ThingSpeak> thingSpeakCall = ManagerAll.getInstance().thingSpeak();
         thingSpeakCall.enqueue(new Callback<ThingSpeak>() {
             @Override
             public void onResponse(Call<ThingSpeak> call, Response<ThingSpeak> response) {
                 temp = response.body().getFeeds().get(99).getField1();
-                tempTemp = response.body().getFeeds().get(98).getField1();
                 System.out.println("alınan temp: " + temp);
                 System.out.println("alınan temptemp: " + tempTemp);
                 thingSpeakHum();
@@ -126,28 +126,25 @@ public class Detail extends AppCompatActivity {
         });
     }
 
-    public void thingSpeakHum() {
-       Call<ThingSpeakHum> thingSpeakHumCall = ManagerAll.getInstance().thingSpeakHum();
-       thingSpeakHumCall.enqueue(new Callback<ThingSpeakHum>() {
-           @Override
-           public void onResponse(Call<ThingSpeakHum> call, Response<ThingSpeakHum> response) {
-               hum = response.body().getFeeds().get(98).getField2();
-               tempHum = response.body().getFeeds().get(99).getField2();
-               if (temp.equals(tempHum) && hum.equals(tempHum)) {
-                   System.out.println("aynı veri geldi...");
-               } else {
-                   System.out.println("xxxalınan hum: " + hum   );
-                   System.out.println("xxxalınan temphum: " + tempHum);
-                   System.out.println("xxxalınan temp: " + temp);
-                   System.out.println("xxxalınan temptemp: " + tempTemp);
-                   thingSpeakOut();
-               }
-           }
-           @Override
-           public void onFailure(Call<ThingSpeakHum> call, Throwable t) {
 
-           }
-       });
+    public void thingSpeakHum() {
+        Call<ThingSpeakHum> thingSpeakHumCall = ManagerAll.getInstance().thingSpeakHum();
+        thingSpeakHumCall.enqueue(new Callback<ThingSpeakHum>() {
+            @Override
+            public void onResponse(Call<ThingSpeakHum> call, Response<ThingSpeakHum> response) {
+                hum = response.body().getFeeds().get(99).getField2();
+
+                System.out.println("xxxalınan hum: " + hum);
+                System.out.println("xxxalınan temp: " + temp);
+
+                tempAndHum();
+            }
+
+            @Override
+            public void onFailure(Call<ThingSpeakHum> call, Throwable t) {
+
+            }
+        });
     }
 
     public void thingSpeakOut() {
@@ -157,7 +154,6 @@ public class Detail extends AppCompatActivity {
             public void onResponse(Call<ThingSpeakOut> call, Response<ThingSpeakOut> response) {
                 out = response.body().getFeeds().get(99).getField3();
                 System.out.println("alınan output: " + out);
-                tempAndHumAndOut();
             }
 
             @Override
@@ -172,10 +168,10 @@ public class Detail extends AppCompatActivity {
         call.enqueue(new Callback<Result>() {
             @Override
             public void onResponse(Call<Result> call, Response<Result> response) {
-                 String word = "success";
-                 if(word.equals(response.body().getResult())) {
-                     System.out.println("kayıt başarılı");
-                 }
+                String word = "success";
+                if (word.equals(response.body().getResult())) {
+                    System.out.println("kayıt başarılı");
+                }
             }
 
             @Override
@@ -185,23 +181,5 @@ public class Detail extends AppCompatActivity {
         });
     }
 
-    public void tempAndHumAndOut() {
-        Call<Result> call = ManagerAll.getInstance().tempAndHumAndOut(temp, hum, out);
-        call.enqueue(new Callback<Result>() {
-            @Override
-            public void onResponse(Call<Result> call, Response<Result> response) {
-                String word = "success";
-                if(word.equals(response.body().getResult())) {
-                    System.out.println("kayıt başarılı");
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Result> call, Throwable t) {
-                System.out.println("hata var: " + t.getMessage());
-            }
-        });
-
-    }
-
 }
+
